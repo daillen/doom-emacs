8d40e5a3d32fa55564dd95852d10082bc2ac2e64
Andre Vaillant <andre.v712@gmail.com>
2024-05-07 20:42:29 -0300

    First commit

---
+++ custom.el
@@ -0,0 +1,20 @@
+;;; custom.el ---                                    -*- lexical-binding: t; -*-
+
+;; Copyright (C) 2024  Andre Vaillant
+
+;; Author: Andre Vaillant <andre@Andres-MBP>
+;; Keywords:
+(custom-set-variables
+ ;; custom-set-variables was added by Custom.
+ ;; If you edit it by hand, you could mess it up, so be careful.
+ ;; Your init file should contain only one such instance.
+ ;; If there is more than one, they won't work right.
+ '(custom-safe-themes
+   '("8d8207a39e18e2cc95ebddf62f841442d36fcba01a2a9451773d4ed30b632443" default))
+ '(magit-todos-insert-after '(bottom) nil nil "Changed by setter of obsolete option `magit-todos-insert-at'"))
+(custom-set-faces
+ ;; custom-set-faces was added by Custom.
+ ;; If you edit it by hand, you could mess it up, so be careful.
+ ;; Your init file should contain only one such instance.
+ ;; If there is more than one, they won't work right.
+ )
